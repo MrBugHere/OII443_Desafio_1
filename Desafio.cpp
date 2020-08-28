b9a41ec14303c0b2898b1c8df9129a0f6a33f63e
Jose <jose.garcia.z@mail.pucv.cl>
2020-08-28 16:43:00 -0400

    se agregaron unas funciones

--- Desafio.cpp
+++ Desafio.cpp
@@ -5,19 +5,37 @@ class Container
 {
 public:
     std::list<float> contents; // peso en Kgrs. de cada item que se coloca
-    int currentWeight;   // suma de todo los pesos contenidos
+    float currentWeight;   // suma de todo los pesos contenidos
+
+    void insertItem(float item);
 };
+
+void Container::insertItem(float item){ //Inserta el item dentro del container, no importa si hay limite
+    currentWeight += item;
+    contents.push_front(item);
+}
 
 struct Action
 {
-    int noContainer;
-    float item; 
+    int noContainer; //Numero de Container;
+    float item; //Peso del obejto
 };
 
-class state
+class State
 {
 public:
     int rank; //Valor del estado segun heuristica
     std::list<float> itemQueue; //Pesos que faltan por colocar
     std::list<Container> containers; //lista de Containers
+};
+
+bool isValidState(State S, float max){
+    std::list<Container>::iterator it;
+
+    for(it = S.containers.begin(); it!=S.containers.end(); ++it){
+        if( it.currentWeight > max){
+            return false;
+        }
+    };
+    return true;
 };
